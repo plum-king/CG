848be56006983928dcba2e81442f78cf90caae12
plum-king <jeesoo0908@gmail.com>
2021-08-29 23:03:32 +0900

    hide secret key

--- CrystalGallery/CrystalGallery/settings.py
+++ CrystalGallery/CrystalGallery/settings.py
@@ -31,9 +31,6 @@ for key, value in secrets.items():
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uk)*09fkfylba#+w)lhn0o86=lgycd^de(^$inqiww_t%*67zx'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
