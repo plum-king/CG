e3970baefefb18121fefe6d1b94a9009299f6abb
plum-king <77599304+plum-king@users.noreply.github.com>
2021-08-29 22:59:25 +0900

    Update settings.py

--- CrystalGallery/CrystalGallery/settings.py
+++ CrystalGallery/CrystalGallery/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uk)*09fkfylba#+w)lhn0o86=lgycd^de(^$inqiww_t%*67zx'
+SECRET_KEY = ''
+#mod SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
